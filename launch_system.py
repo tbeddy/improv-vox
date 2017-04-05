610fd8b9deb6d154ebab310c68500bf360f17dd6
Tim Bedford <tmbdfd@gmail.com>
2017-04-04 21:14:15 -0400

    Reference each of the components' new locations

--- launch_system.py
+++ launch_system.py
@@ -4,9 +4,9 @@ Run this file to start the system.
 
 import subprocess
 
-max_commands = ["open", "-a", "Max", "P.maxpat"]
-csound_commands = ["csound", "-o", "dac", "-d", "Q.csd"]
-python_commands = ["python", "f.py"]
+max_commands = ["open", "-a", "Max", "ear/P.maxpat"]
+csound_commands = ["csound", "-o", "dac", "-d", "mouth/Q.csd"]
+python_commands = ["python", "brain/f.py"]
 
 launch_max = subprocess.run(max_commands)
 launch_csound = subprocess.Popen(csound_commands)
